11d82f491ad9d28f3bebe5a45579f0f1810caf21
Timur <s80516@beuth-hochschule.de>
2021-06-14 12:47:34 +0200

    no message

--- Project/backend/user/userRoute.js
+++ Project/backend/user/userRoute.js
@@ -22,7 +22,7 @@ router.get('/', isAuthenticated, function (req, res, next) {
 
 router.get("/find", isAuthenticated, async function (req, res, next) {
     console.log("Trying to find User by Id");
-    userService.findUserById(req.body, function (err, user) {
+    userService.findUserBy(req.body, function (err, user) {
         if (user) {
             console.log("Found User by Id");
             res.send(user);
