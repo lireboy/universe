e6da148c2fb9d3550239d600be6656db0b21dea3
Timur <s80516@beuth-hochschule.de>
2021-06-24 18:11:37 +0200

    no message

--- Project/backend/user/userService.js
+++ Project/backend/user/userService.js
@@ -48,8 +48,6 @@ function getUsers(callback) {
 
 function findUserBy(searchUserID, callback) {
     console.log("UserService: find user by Id: " + searchUserID);
-
-    if (!searchUserID.userId) {
     if (!searchUserID) {
         callback("userId is missing");
     } else {
