b729bf90c6bab5dee3fa6b9ba64cdadb723f3af1
pddx1 <87027845+pddx1@users.noreply.github.com>
2021-07-11 13:41:50 +0200

    Functioning Info customization + Styled Logout button

--- Project/frontend/src/js/Profile.js
+++ Project/frontend/src/js/Profile.js
@@ -3,6 +3,9 @@ import React from 'react';
 import profilepic from '../img/profilepicture.png';
 import settings from '../img/svg/settings.svg';
 import { Link } from "react-router-dom";
+import { useState } from "react";
+const axios = require("axios");
+
 
 let games = [
   {
@@ -43,43 +46,65 @@ let games = [
   }
 ];
 
-const profile = (props) => {
-    return(
-      <div className="profile_overview">
-        <div className="profile">
-          <div className="left">
-              <img className="profile-picture" id="profile_user_Picture" src={profilepic} alt=""/> 
-              <Link className="icon-settings" to="/settings" onClick={() => props.setActiveTab("settings")}>
-                <img className={`icon-settings ${props["activeTab"] === "settings" ? "active" : null}`} src={settings} alt="" />
-              </Link>    
-          </div>
-          <div className="right">
-              <p className="big">{props.activeUser.name}</p>             
-              <p className="medium">Info:</p>
-              <p className="small">{props.activeUser.info}</p> 
-              <p className="small">{props.activeUser.email}</p>   
-          </div>
+const Profile = (props) => {
+  const [info, setInfo] = useState(props.activeUser.info);
+
+  function formPreventDefault(e) {
+    e.preventDefault();
+    console.log("trying to edit user");
+    axios.patch("http://localhost:8080/user/" + props.activeUser.userId, {
+      headers: {
+        'content-type': 'application/json',
+      },
+      "info": info
+    })
+      .then(res => {
+        console.log(res.data);
+        props.activeUser.info = info;
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  return (
+    <div className="profile_overview">
+      <div className="profile">
+        <div className="left">
+          <img className="profile-picture" id="profile_user_Picture" src={profilepic} alt="" />
+          <Link className="icon-settings" to="/settings" onClick={() => props.setActiveTab("settings")}>
+            <img className={`icon-settings ${props["activeTab"] === "settings" ? "active" : null}`} src={settings} alt="" />
+          </Link>
         </div>
-        <div id ="recentlyPlayed">
-          <p id="recent">Recently Played:</p>
-          <ul id="gameList">
-            {games.map((game) => {
-              return <li className="profile_game_container" key={game.id}>
-                <img src={game.img} alt=""/>
-                <div className="profile_game_info">
-                  <p className="profile_game_title">{game.title}</p>
-                  <div className="profile_game_specific">
-                    <p className="profile_game_gametime">{game.hours} ingesamt</p>
-                    <p className="profile_game_lastplayed">Zuletzt gespielt am 13. Mai</p>
-                  </div>
-                </div>
-              </li>
-            })}
-          </ul>
+        <div className="right">
+          <p className="big">{props.activeUser.name}</p>
+          <p className="medium">Info:</p>
+          <form onSubmit={formPreventDefault}>
+          <input id="info" placeholder="Default Text" type="text" name="name" value={info} onChange={(e) => setInfo(e.target.value)} />
+          </form>
+          <p className="small">{props.activeUser.email}</p>
         </div>
       </div>
-    )
-    
+      <div id="recentlyPlayed">
+        <p id="recent">Recently Played:</p>
+        <ul id="gameList">
+          {games.map((game) => {
+            return <li className="profile_game_container" key={game.id}>
+              <img src={game.img} alt="" />
+              <div className="profile_game_info">
+                <p className="profile_game_title">{game.title}</p>
+                <div className="profile_game_specific">
+                  <p className="profile_game_gametime">{game.hours} ingesamt</p>
+                  <p className="profile_game_lastplayed">Zuletzt gespielt am 13. Mai</p>
+                </div>
+              </div>
+            </li>
+          })}
+        </ul>
+      </div>
+    </div>
+  )
+
 }
 
-export default profile;
+export default Profile;
