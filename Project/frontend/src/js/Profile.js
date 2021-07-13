4d42060796a64a9efad5e2ed2cee17b6f910b2e9
pddx1 <87027845+pddx1@users.noreply.github.com>
2021-07-13 14:56:46 +0200

    Custom Image Upload

--- Project/frontend/src/js/Profile.js
+++ Project/frontend/src/js/Profile.js
@@ -10,6 +10,7 @@ const Profile = (props) => {
 
   const [info, setInfo] = useState(props.activeUser.info);
   const [username, setUsername] = useState(props.activeUser.name);
+  const [img, setImg] = useState(props.activeUser.img);
 
   function formPreventDefault(e) {
     e.preventDefault();
@@ -19,12 +20,14 @@ const Profile = (props) => {
         'content-type': 'application/json',
       },
       "info": info,
-      "name": username
+      "name": username,
+      "img": img
     })
       .then(res => {
         console.log(res.data);
         props.activeUser.info = info;
         props.activeUser.name = username;
+        props.activeUser.img = img;
       })
       .catch(err => {
         console.log(err);
@@ -43,20 +46,48 @@ const Profile = (props) => {
     }
   }
 
+  const handleImageChange = (e) =>  {
+    const selected = e.target.files[0];
+    const allowed_types = ["image/png", "image/jpeg", "image/jpg", "image/webp", "image/gif"];
+    if(selected && allowed_types.includes(selected.type)) {
+      let reader = new FileReader();
+      reader.onloadend = () => {
+        setImg(reader.result);
+      }
+      reader.readAsDataURL(selected);
+    } else {
+      console.log("file not supported");
+    }
+  }
+
   return(
     <div className="profile_overview">
       <div className="profile">
-        <div className="left">
-          <img className="profile-picture" id="profile_user_Picture" src={profilepic} alt="" />
+      <div className="left">
+        <div className="profile-picture" style={{background: img ? `url("${img}") no-repeat center/cover` : "#131313"}}>
+          {!img && (
+            <>
+            <img className="profile-picture" id="profile_user_Picture" src={img} alt="" />  
+            </>       
+          )}
+          {/* <img className="profile-picture" id="profile_user_Picture" src={profilepic} alt="" />
+           */}
+           </div>
         </div>
         <form onSubmit={formPreventDefault} className="profile-right">
           <input type="text" className="big" name="username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
           <p className="medium">Info:</p>
           <textarea rows="8" className="info" type="text" name="info" value={info} onChange={(e) => setInfo(e.target.value)}>
           </textarea>
+          <div className="buttonPos">
+          <label for ="fileupload" className="fileupload">Select Image</label>
+          <input type="file" id="fileupload" onChange={handleImageChange} />
           <Button variant="success" type="submit" >Update</Button>
+          </div>
         </form>
+        
       </div>
+      
       <div id ="recentlyPlayed">
         <p id="recent">Recently Played:</p>
         <ul id="gameList">
